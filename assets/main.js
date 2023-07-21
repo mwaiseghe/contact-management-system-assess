85319f0b1ee25454ec707ff5348c00a82005bb49
mwaiseghe <mwaiseghe23@gmail.com>
2023-07-21 11:28:49 +0300

    fixed add user issue

--- assets/main.js
+++ assets/main.js
@@ -16,6 +16,14 @@ class Users {
         this.password = password;
     }
 
+    static getLastId() {
+        let lastId = 0;
+        if (users_data.length > 0) {
+            lastId = users_data[users_data.length - 1].id;
+        }
+        return lastId;
+    }
+
     static createUser(username, password) {
         let user = new Users(username, password);
         users_data.push(user);
@@ -32,7 +40,28 @@ class Users {
             alert('Invalid username or password');
         }
     }
+
+    static user_events_listeners() {
+        let new_user_form = document.querySelector('#new-user-form');
+
+        new_user_form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let username = document.querySelector('#new-username').value;
+            let password = document.querySelector('#new-password').value;
+            Users.createUser(username, password);
+        });
+
+        let login_form = document.querySelector('#login_form');
+
+        login_form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let username = document.querySelector('#username').value;
+            let password = document.querySelector('#password').value;
+            Users.login(username, password);
+        });
+        }
 }
+
 
 class Contacts {
     constructor(name, phone, email) {
@@ -41,22 +70,20 @@ class Contacts {
         this.email = email;
     }
 
-    static displayContacts(user_id) {
-        let user_contacts = contacts_data.filter(
-            contact => contact.user_id == user_id
-            );
+    static displayContacts() {
+        let user_contacts = contacts_data
         contact_list.innerHTML = '';
         user_contacts.forEach(contact => {
             contact_list.innerHTML += `
             <li>
                 <div class="contact">
                     <div class="contact-info">
-                        <h3>John Doe</h3>
-                        <p> <span>Phone:</span> 1234567890</p>
-                        <p> <span>Email:</span> janedoe@mail.com </p>
+                        <h3>${contact.name}</h3>
+                        <p> <span>Phone:</span> ${contact.phone} </p>
+                        <p> <span>Email:</span> ${contact.email} </p>
                     </div>
                     <div class="contact-action">
-                        <img src="assets/images/1.jpg" alt="">
+                        <img src="" alt="">
                     </div>
                 </div>
             </li>
@@ -68,7 +95,7 @@ class Contacts {
         let contact = new Contacts(name, phone, email, user_id);
         contacts_data.push(contact);
         localStorage.setItem('contacts', JSON.stringify(contacts_data));
-        Contacts.displayContacts(user_id);
+        Contacts.displayContacts();
         alert('Contact added successfully');
     }
 
@@ -77,7 +104,7 @@ class Contacts {
         let contact = contacts_data.filter(contact => contact.name == contact_name);
         contacts_data.splice(contacts_data.indexOf(contact), 1);
         localStorage.setItem('contacts', JSON.stringify(contacts_data));
-        Contacts.displayContacts(user_id);
+        Contacts.displayContacts();
         alert('Contact deleted successfully');
     }
 
@@ -88,5 +115,25 @@ class Contacts {
         }
         return lastId;
     }
+
+    static contact_events_listeners() {
+        contact_form = document.querySelector('#contact-form');
+        contact_form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let name = document.querySelector('#contact-name').value;
+            let phone = document.querySelector('#contact-phone').value;
+            let email = document.querySelector('#contact-email').value;
+            let user_id = JSON.parse(localStorage.getItem('user'))[0].id;
+            Contacts.add_contact(name, phone, email, user_id);
+        });
+    }
 }
+
+
+
+
+
+// Users.user_events_listeners();
+Contacts.contact_events_listeners();
+Contacts.displayContacts(JSON.parse(localStorage.getItem('user'))[0].id);
 
